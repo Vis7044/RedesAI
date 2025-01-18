b107d66398e8b491d6214adc7c36cc88d274483d
Sahilsajar <sahilsajar13890@gmail.com>
2025-01-19 00:50:07 +0530

    responsive of result page

--- frontend/src/Chart.jsx
+++ frontend/src/Chart.jsx
@@ -1,35 +1,38 @@
-import React, { useState } from 'react';
-import ReactApexChart from 'react-apexcharts';
+import React, { useState } from "react";
+import ReactApexChart from "react-apexcharts";
 
-
-const Chart = ({positive,negative,neutral}) => {
+const Chart = ({ positive, negative, neutral }) => {
   const [state, setState] = useState({
-    series: [Number(positive), Number(negative), 100.0-(Number(positive)+Number(negative))],
+    series: [
+      Number(positive),
+      Number(negative),
+      100.0 - (Number(positive) + Number(negative)),
+    ],
     options: {
       chart: {
         width: 480,
-        type: 'pie',
+        type: "pie",
       },
-      labels: ['Positive', 'Negative', 'Neutral'],
-      colors: ['#8ADE80', '#FF4C72', '#4B5563'],
+      labels: ["Positive", "Negative", "Neutral"],
+      colors: ["#8ADE80", "#FF4C72", "#4B5563"],
       responsive: [
         {
-          breakpoint: 480,
+          breakpoint: 600,
           options: {
             chart: {
-              width: 200,
+              width: 350,
             },
             legend: {
-              position: 'bottom',
+              position: "bottom",
             },
           },
         },
       ],
     },
   });
-  
+
   return (
-    <div>
+    <div className="flex justify-center md:block">
       <ReactApexChart
         options={state.options}
         series={state.series}
