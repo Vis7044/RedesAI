20828a94a6e152c353e57e90ab1cb1a224cae498
Sahilsajar <sahilsajar13890@gmail.com>
2025-04-11 19:03:54 +0530

    second

---
+++ nodeBackend/controllers/authController.js
@@ -0,0 +1,43 @@
+const User = require("../models/User");
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Please provide name, email and password" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.login = () => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ message: "Please provide email and password" });
+  }
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ message: "Invalid credentials" });
+      }
+      return bcrypt.compare(password, user.password).then((isMatch) => {
+        if (!isMatch) {
+          return res.status(401).json({ message: "Invalid credentials" });
+        }
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+          expiresIn: "1h",
+        });
+        res.status(200).json({ token, userId: user._id });
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ message: "Server error" });
+    });
+};
