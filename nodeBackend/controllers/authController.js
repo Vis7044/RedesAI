a8ec141d6c23889f6299d7ad035f17a1dd47cd6e
Sahilsajar <sahilsajar13890@gmail.com>
2025-04-11 19:35:42 +0530

    authcontroller

--- nodeBackend/controllers/authController.js
+++ nodeBackend/controllers/authController.js
@@ -1,4 +1,7 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+require("dotenv").config();
 exports.register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -7,37 +10,53 @@ exports.register = async (req, res) => {
         .status(400)
         .json({ message: "Please provide name, email and password" });
     }
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const passwordHash = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      name,
+      email,
+      password: passwordHash,
+    });
+    await newUser.save();
+    return res.status(201).json({
+      message: "User registered successfully",
+      success: true,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error" });
   }
 };
 
-exports.login = () => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return res
-      .status(400)
-      .json({ message: "Please provide email and password" });
-  }
-
-  User.findOne({ email })
-    .then((user) => {
-      if (!user) {
-        return res.status(401).json({ message: "Invalid credentials" });
-      }
-      return bcrypt.compare(password, user.password).then((isMatch) => {
-        if (!isMatch) {
-          return res.status(401).json({ message: "Invalid credentials" });
-        }
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-          expiresIn: "1h",
-        });
-        res.status(200).json({ token, userId: user._id });
-      });
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).json({ message: "Server error" });
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Please provide email and password" });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "Invalid credentials", success: false });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ message: "Invalid credentials", success: false });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
     });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
 };
