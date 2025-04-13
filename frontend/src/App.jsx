8d0cb02193b7cce93819de9d12deeef8a2cdbb06
Vis7044 <singhvishal7044@gmail.com>
2025-04-13 23:32:15 +0530

    improved some ui glitch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,10 @@ import Profile from './pages/Profile'
 function App() {
 
   return (
-    <div className="App min-h-screen container bg-black mx-auto inter-karla">
+    <div className="App min-h-screen container mx-auto inter-karla" style={{
+      backgroundImage:
+        'radial-gradient( #091D0E 0%,#0c0c0b 40%,  #0f0f0f 100%)',
+    }}>
       <BrowserRouter>
       <Header />
       <ToastContainer/>
